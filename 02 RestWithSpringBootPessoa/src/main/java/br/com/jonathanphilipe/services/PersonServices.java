a9d2b85e24a7eb88feb3a5b02acd36a88c53204c
Jonathan Philipe <sicrano3000@gmail.com>
2020-08-04 14:29:22 -0300

    Estrutura Rest

---
+++ 02 RestWithSpringBootPessoa/src/main/java/br/com/jonathanphilipe/services/PersonServices.java
@@ -0,0 +1,61 @@
+package br.com.jonathanphilipe.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.jonathanphilipe.model.Person;
+
+@Service
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+	}
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Jonathan");
+		person.setLastName("Crespo");
+		person.setAddress("São Gonçalo - RJ");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		
+		for (int i=0; i<8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person Name " + i);
+		person.setLastName("Last Name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+}
