8c19db69798033d9de13c7b01e8a4c06cdb2211f
Jonathan Philipe <sicrano3000@gmail.com>
2020-08-23 12:19:24 -0300

    migration

---
+++ 06 RestWithSpringBootMigrations/src/main/resources/db/migration/V2__Population_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES (1,'São Gonçalo, RJ','Jonathan','Male','Crespo'),
+(2,'São Gonçalo - RJ','Thairine','Female','Crespo'),
+(3,'São Gonçalo - RJ','Caroline','Female','Crespo'),
+(6,'São Gonçalo - RJ','Nala','Female','Crespo'),
+(7,'São Gonçalo - RJ','Vini','Male','Crespo');
